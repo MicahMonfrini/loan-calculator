cc346be408c2acfdf3fd88c3b770961c8bbaba80
Micah Monfrini <ffrock905@gmail.com>
2018-12-12 17:53:22 -0600

    add input value and monthly payment algorithms

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ function calculateResults(e) {
   const totalPaymentEl = document.getElementById('total-payment');
   const totalInterestEL = document.getElementById('total-interest');
 
+  // input value algorithms (converted to decimal values)
+  const principle = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // monthly payment algorithm
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principle*x*calculatedInterest)/(x-1);
+
   // prevent default form submission
   e.preventDefault();
 }
